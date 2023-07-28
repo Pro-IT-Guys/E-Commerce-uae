f91feade5ad5005fd56b7c2af7073ed9d45fa0e1
Md Imran Hossen <freelancerimran81@gmail.com>
2023-07-28 10:33:00 +0600

    login cancel button

--- src/components/AuthModal/LoginModal.js
+++ src/components/AuthModal/LoginModal.js
@@ -6,6 +6,7 @@ import {
 } from "@mui/material";
 import { DialogAnimate } from "../animate";
 import LoginForm from "../Auth/LoginForm";
+import CancelIcon from '@mui/icons-material/Cancel';
 
 // ----------------------------------------------------------------------
 
@@ -20,7 +21,13 @@ export default function LoginFormModal({ open, onClose }) {
 
   return (
     <DialogAnimate maxWidth="sm" open={open} onClose={onClose} >
-      <LoginForm   onClose={onClose}/>
+
+      <div
+        onClick={onClose}
+        className="flex justify-end py-3 pr-4 cursor-pointer text-secondary">
+        <CancelIcon fontSize="large"/>
+      </div>
+      <LoginForm onClose={onClose} />
     </DialogAnimate>
   );
 }
